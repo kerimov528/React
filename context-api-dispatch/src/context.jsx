5bdfd1fca547db5ca89b06a35a21ae2ab1a51a25
kerimov528 <kerimov.anar528@gmail.com>
2022-09-03 16:44:26 -0700

    Completed

--- context-api-dispatch/src/context.jsx
+++ context-api-dispatch/src/context.jsx
@@ -1,46 +1,41 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 
 const UserContext = React.createContext();
 
 const reducer = (state, action) => {
-
+    // eslint-disable-next-line default-case
     switch (action.type) {
         case "DELETE_USER":
             return {
                 ...state,
                 users: state.users.filter(user => action.payload !== user.id)
             }
+        case "ADD_USER":
+            return {
+                ...state,
+                users: [...state.users, action.payload]
+            }
+
+
     }
 
 }
 
 export class UserProvider extends Component {
     state = {
-        users: [
-            {
-                id: 1,
-                name: "Anar",
-                email: "anarrkerimli@gmail.com",
-                age: 20
-            }
-            ,
-            {
-                id: 2,
-                name: "Turan",
-                email: "anarrkerimli@gmail.com",
-                age: 44
-            },
-            {
-                id: 3,
-                name: "Ali",
-                email: "anarrkerimli@gmail.com",
-                age: 20
-            }
-        ],
+        users: [],
         dispatch: action => {
             this.setState(state => reducer(state, action))
         }
+    }
+
+    componentDidMount = async ()=> {
+          const response = await axios.get("http://localhost:3000/users")
+           this.setState({
+            users: response.data
+          })
     }
 
     render() {
